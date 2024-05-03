4fb574b45f6ec5d0cfb9720ce4831835a2c0ae5f
Cairo <cairoignacio2015@gmail.com>
2024-05-02 20:12:37 -0600

    Contacto Formulario Terminado

--- src/components/generals/Contact.vue
+++ src/components/generals/Contact.vue
@@ -5,33 +5,66 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-6 izquierda">
-                <img src="../../assets/images/form-contact-es.png" alt="formulario"/>
+            <div class="col-lg-6 izquierda order-2 order-lg-1">
+                <img class="img-fluid" src="../../assets/images/form-contact-es.png" alt="formulario"/>
             </div>
-                <div class="col-6 derecha">
+                <div class="col-lg-6 derecha order-1 order-lg-2">
                     <form class="contenedor-form">
-                    <div class="mb-3">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre y apellido"> 
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Nombre de empresa">
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Correo electrónico">
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Teléfono">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
-                        <label class="form-check-label" for="exampleCheck1">Estrategias de marketing orientadas al crecimiento.</label>
-                    </div>
-                    <hr>
-                    <button type="submit" class="btn btn-danger">Trabajemos</button>
-                    </form>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre y apellido"/> 
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Nombre de empresa">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Correo electrónico">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Teléfono">
+                            </div>   
+                            
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
+                                        <label class="form-check-label" for="exampleCheck1">Desarrollo de e-commerce y websites informativos</label>
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
+                                        <label class="form-check-label" for="exampleCheck1">Subcontratación de software</label>
+                                    </div>
+                                   </div>
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
+                                        <label class="form-check-label" for="exampleCheck1">Desarrollo de software a la medida y automatización</label>
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
+                                        <label class="form-check-label" for="exampleCheck1">Diseño estratégico de marca</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="mb-3">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
+                                        <label class="form-check-label" for="exampleCheck1">Estrategias de marketing orientadas al crecimiento.</label>
+                                    </div>
+                                   </div>
+                            </div>
+                            <hr>
+                            <div class="mb-3">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" placeholder="name@example.com">
+                                <label class="form-check-label" for="exampleCheck1">Al hacer clic aquí, confirmas tu autorización conforme a los términos de uso y política de privacidad.</label>
+                            </div> 
+                            <button type="submit" class="btn btn-danger">Trabajemos</button>
+                            </form>
+                        
+                        
                 </div>
-        </div>
-    </div>
+            </div>
+        </div>   
 </template>
 
 <style scoped>
@@ -41,14 +74,55 @@
 }
 
 .derecha{
-    margin: 10rem 0rem;
+    margin-bottom: 3rem;
    
 }
 
 .contenedor-form{
-    padding: 45px 35px;
+    padding: 45px 32px;
     border-radius: 12px;
     border: 2px solid #343d7e;
+    background-color: #1e202d;
 }
 
+hr{
+    border-color: white;
+    border-width: 2px;
+    opacity: 0.60;
+}
+
+.form-control {
+    background-color: transparent;
+    color: white;
+}
+
+::placeholder {
+        color: rgba(255, 255, 255, 0.5); 
+    }
+
+.form-control:focus {
+        border-color: #f41052;
+        box-shadow: none;
+    }    
+
+label {
+    display: inline;
+    margin-left: 8px;
+}
+
+
+.btn-danger{
+    background-color: #f41052;
+} 
+
+.btn-danger:hover{
+    background-color: #000315;
+    color: #f41052;
+} 
+
+@media (max-width:768px){
+.izquierda img{ 
+    height: auto;
+  }
+}
 </style>
